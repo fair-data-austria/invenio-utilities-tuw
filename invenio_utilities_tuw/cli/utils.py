394feb0cde4a03e7395393b10c14bb5359fbe068
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2021-01-29 10:17:00 +0100

    global: update licenses

--- invenio_utilities_tuw/cli/utils.py
+++ invenio_utilities_tuw/cli/utils.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2021 TU Wien.
+#
+# Invenio-Utilities-TUW is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
 """Utilities for the CLI commands."""
 
 import json
--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020 TU Wien.
+# Copyright (C) 2020-2021 TU Wien.
 #
 # Invenio-Utilities-TUW is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
